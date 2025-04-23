70e798da2ac5fc3e093d76aef16887d6c145fcd3
Academy <aper29012006@gmail.com>
2025-04-23 20:17:57 +0300

    Create models: authors and books

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(verbose_name="Полное имя автора", max_length=80)
+    bio = models.TextField(verbose_name="Краткая биография автора")
+
+class Book(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=120)
+    desc = models.TextField(verbose_name="Описание")
+    price = models.FloatField(verbose_name="Цена")
+    publication_date = models.DateField(verbose_name="Дата публикации")
+
+    date_create = models.DateTimeField(verbose_name="Дата добавления", auto_now_add=True)
+    date_edit = models.DateTimeField(verbose_name="Дата изменения", auto_now=True)
+
+    authors = models.ManyToManyField(Author, related_name="books", verbose_name="Автор(ы)")
+
