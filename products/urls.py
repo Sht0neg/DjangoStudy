516bf5e903c6a5d709bbcd6eb1f32864f801729c
Academy <aper29012006@gmail.com>
2025-05-05 18:43:13 +0300

    Changed the main page view function to class

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from products.views import index, catalog
+from products.views import IndexListView, catalog
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", IndexListView.as_view(), name="index"),
     path("catalog/", catalog, name="catalog"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from products.models import Book
-def index(req):
-    books = Book.objects.all()
-    return render(req, 'index.html', {"books" : books})
+from django.views.generic import ListView
+
+class IndexListView(ListView):
+    model = Book
+    template_name = "index.html"
+    context_object_name = "books"
 
 def catalog(req):
     return render(req, "catalog.html")
