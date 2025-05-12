c3589476b3e825f81987950a5217b2d4edf6c734
Academy <aper29012006@gmail.com>
2025-05-12 20:39:32 +0300

    Set up redirection for authorization application

--- user/views.py
+++ user/views.py
@@ -5,17 +5,14 @@ from django.contrib.auth.models import User
 
 class UserLoginView(LoginView):
     template_name = "login.html"
-    success_url = "/user/account"
 
 class RegistrationView(CreateView):
     form_class = UserCreationForm
     template_name = "registration.html"
-    success_url = "/user/account"
 
 class AccountView(DetailView):
     model = User
     template_name = "account.html"
-    context_object_name = "account"
 
 class UserLogoutView(LogoutView):
     pass
